cc8e27c22026de8cbc443badf6323198ee9d40f9
at055612 <at055612@gmail.com>
2018-11-16 22:59:07 +0000

    Tidy up logging, add more colour

--- bash/send_to_stroom.sh
+++ bash/send_to_stroom.sh
@@ -25,10 +25,23 @@ readonly DELETE_AFTER_SENDING=${_arg_delete_after_sending}
 readonly PRETTY=${_arg_pretty}
 readonly FILE_REGEX=${_arg_file_regex:-.*/.*\.log}
 
-## Configure other constants
+# Configure other constants
 readonly LOCK_FILE=${LOG_DIR}/$(basename "$0").lck
 readonly SLEEP=$((RANDOM % (MAX_SLEEP+1)))
 readonly THIS_PID=$$
+
+
+echo_info() {
+    echo -e "${INFO_PREFIX}${1}"
+}
+
+echo_warn() {
+    echo -e "${WARN_PREFIX}${1}"
+}
+
+echo_error() {
+    echo -e "${ERROR_PREFIX}${1}"
+}
 
 # Shell colour constants for use in 'echo -e'
 setup_echo_colours() {
@@ -39,6 +52,8 @@ setup_echo_colours() {
         GREEN=''
         YELLOW=''
         BLUE=''
+        MAGENTA=''
+        CYAN=''
         LGREY=''
         DGREY=''
         NC='' # No Color
@@ -47,6 +62,8 @@ setup_echo_colours() {
         GREEN='\033[1;32m'
         YELLOW='\033[1;33m'
         BLUE='\033[1;34m'
+        MAGENTA='\033[1;35m'
+        CYAN='\033[1;36m'
         LGREY='\e[37m'
         DGREY='\e[90m'
         NC='\033[0m' # No Color
@@ -57,27 +74,30 @@ configure_curl() {
     CURL_OPTS=""
     if [ "${SECURE}" = "off" ]; then
         CURL_OPTS="${CURL_OPTS} -k"
-        echo -e "${YELLOW}Warn:${NC} Running in insecure mode where we do not check SSL certificates."
+        echo_info "Running in insecure mode where we do not check SSL certificates."
     fi
     
     if [ ! "x${CERT}" = "x" ]; then
         CURL_OPTS="${CURL_OPTS} --cert ${CERT}"
-    fi
 
-    if [ ! "x${CERT_TYPE}" = "x" ]; then
-        CURL_OPTS="${CURL_OPTS} --cert-type ${CERT_TYPE}"
     fi
 
     if [ ! "x${KEY}" = "x" ]; then
         CURL_OPTS="${CURL_OPTS} --key ${KEY}"
-    fi
 
-    if [ ! "x${KEY_TYPE}" = "x" ]; then
-        CURL_OPTS="${CURL_OPTS} --key-type ${KEY_TYPE}"
+        if [ ! "x${KEY_TYPE}" = "x" ]; then
+            CURL_OPTS="${CURL_OPTS} --key-type ${KEY_TYPE}"
+        fi
     fi
 
     if [ ! "x${CACERT}" = "x" ]; then
         CURL_OPTS="${CURL_OPTS} --cacert ${CACERT}"
+    fi
+
+    if [ ! "x${CERT}" = "x" ] || [ ! "x${CACERT}" = "x" ] ; then
+        if [ ! "x${CERT_TYPE}" = "x" ]; then
+            CURL_OPTS="${CURL_OPTS} --cert-type ${CERT_TYPE}"
+        fi
     fi
 
     if [ ! "x${CURL_OPTS}" = "x" ]; then
@@ -90,22 +110,22 @@ get_lock() {
         LOCK_FILE_PID=$(head -n 1 "${LOCK_FILE}")
         if ps -p "$LOCK_FILE_PID" > /dev/null
         then
-            echo -e "${RED}Error:${NC} This script is already running is already running as ${LOCK_FILE_PID}! Exiting."
+            echo_warn "This script is already running is already running as ${CYAN}${LOCK_FILE_PID}${NC}! Exiting."
             exit 0
         else 
-            echo -e "${YELLOW}Warn:${NC} Found old lock file! Did a previous run of this script fail? I will delete it and create a new one."
-            echo -e "${GREEN}Info:${NC} Creating a lock file for ${THIS_PID}"
+            echo_warn "Found old lock file! Did a previous run of this script fail? I will delete it and create a new one."
+            echo_info "Creating a lock file for ${CYAN}${THIS_PID}${NC}"
             echo "$$" > "${LOCK_FILE}"
         fi
     else
-        echo -e "${GREEN}Info:${NC} Creating a lock file for ${THIS_PID}"
+        echo_info "Creating a lock file for ${CYAN}${THIS_PID}${NC}"
         echo "$$" > "${LOCK_FILE}"
     fi
 }
 
 send_files() {
     if [ ${MAX_SLEEP} -ne 0 ]; then
-        echo -e "${GREEN}Info:${NC} Will sleep for ${SLEEP}s to help balance network traffic"
+        echo_info "Will sleep for ${SLEEP}s to help balance network traffic"
         sleep ${SLEEP}
     fi
 
@@ -116,9 +136,7 @@ send_files() {
     #echo "All files:"
     #find "${LOG_DIR}"
 
-    echo -e "\n${GREEN}Info:${NC} Sending files to [${BLUE}${STROOM_URL}${NC}]"
-    echo -e "${GREEN}Info:${NC} Setting headers [Feed:${BLUE}${FEED}${NC}, System:${BLUE}${SYSTEM}${NC}, Environment:${BLUE}${ENVIRONMENT}${NC}]"
-    echo -e "${GREEN}Info:${NC} Using curl options [${BLUE}${CURL_OPTS}${NC}]"
+    echo_info "Sending files to [${BLUE}${STROOM_URL}${NC}], headers [Feed:${BLUE}${FEED}${NC}, System:${BLUE}${SYSTEM}${NC}, Environment:${BLUE}${ENVIRONMENT}${NC}], curl options [${BLUE}${CURL_OPTS}${NC}]"
 
     # Loop over all files in the lock directory
     for file in ${LOG_DIR}/*; do
@@ -136,7 +154,7 @@ send_files() {
 
 send_file() {
     local -r file=$1
-    echo -e "\n${GREEN}Info:${NC} Sending file ${file}"
+    echo_info "Sending file ${file}"
 
     RESPONSE_HTTP=$(curl \
         ${CURL_OPTS} \
@@ -159,12 +177,12 @@ send_file() {
 
     if [ "${RESPONSE_CODE}" != "200" ]
     then
-        echo -e "${RED}Error:${NC} Unable to send file ${file}, response code was: ${RESPONSE_CODE}, error was :\n${RESPONSE_MSG}"
+        echo_error "Unable to send file ${BLUE}${file}${NC}, response code was: ${BLUE}${RESPONSE_CODE}${NC}, error was :\n${RESPONSE_MSG}"
     else
-        echo -e "${GREEN}Info:${NC} Sent file ${file}, response code was ${RESPONSE_CODE}"
+        echo_info "Sent file ${BLUE}${file}${NC}, response code was ${BLUE}${RESPONSE_CODE}${NC}"
 
         if [ "${DELETE_AFTER_SENDING}" = "on" ]; then
-            echo -e "${YELLOW}Warn:${NC} Deleting successfully sent file ${file}"
+            echo_info "Deleting successfully sent file ${BLUE}${file}${NC}"
             rm "${file}"
         fi
     fi
@@ -172,6 +190,17 @@ send_file() {
 
 main() {
     setup_echo_colours
+
+    # Define echo prefixes for consistent log messages
+    readonly BASE_PREFIX="[${MAGENTA}${FEED}${NC}] [${CYAN}${THIS_PID}${NC}] "
+    readonly INFO_PREFIX="${GREEN}Info${NC}  ${BASE_PREFIX}"
+    readonly WARN_PREFIX="${YELLOW}Warn${NC}  ${BASE_PREFIX}"
+    readonly ERROR_PREFIX="${RED}Error${NC} ${BASE_PREFIX}"
+
+    # For debugging all log levels
+    #echo_info "This is an info test"
+    #echo_warn "This is an warn test"
+    #echo_error "This is an error test"
 
     configure_curl
     get_lock
